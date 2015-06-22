98c589bcf8c6c4a86d31a7b471146200487ba095
jings2015 <jings_since2015@163.com>
2015-06-22 09:49:13 +0800

    test chinese

--- Main.java
+++ Main.java
@@ -1,6 +1,6 @@
 public class Main{
   public static void main(String args[]){
     System.out.println("like this?");
-	System.out.println("change on windows");
+	System.out.println("change on windows");//中文
   }
 }
